388b67edfd2d83b9a9a2ff76d9fee384a8eda7ae
imanol <jjusturi@gmail.com>
2022-06-23 11:13:49 -0500

    Delete database creation:wrench:

--- Database_creation.sql
+++
@@ -1,8 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS PGIS_database;
--- Create administrator
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON `PGIS_database`.* TO 'admin'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
